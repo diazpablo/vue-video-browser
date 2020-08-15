a72c34ad0b04be1dd3eff784bc8ae8a9ae414be3
Pablo Diaz <diazpablo@gmail.com>
2020-08-15 15:01:27 +0200

    getting videos from search w/ axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,41 @@
 <template>
 	<div id="app">
-		App
+		<SearchBar @termChange="onTermChange"></SearchBar>
+		<VideoList :videos="videos" />
 	</div>
 </template>
 <script>
+import axios from 'axios';
+import SearchBar from "@/components/SearchBar";
+import VideoList from "@/components/VideoList";
+
+const API_KEY = 'AIzaSyD_FbN6oqDqtBSL_6ftmV0pLlOTlq0G6gc';
 export default {
-	name: 'App'
+	name: 'App',
+	components: {
+		VideoList,
+		SearchBar
+	},
+	data() {
+		return {
+			videos: []
+		}
+	},
+	methods: {
+		onTermChange(term) {
+			axios.get('https://www.googleapis.com/youtube/v3/search', {
+				params: {
+					key: API_KEY,
+					type: 'video',
+					part: 'snippet',
+					q: term
+				}
+			})
+				.then(({ data }) => {
+					// console.log(data.items);
+					this.videos = data.items
+				})
+		}
+	}
 }
 </script>
